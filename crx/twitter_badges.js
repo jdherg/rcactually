982a5f4cbee4026eef21a0503435b8b010178df7
John Hergenroeder <jdherg@gmail.com>
2016-04-02 00:50:48 -0700

    Change 'RC' to image.

--- crx/twitter_badges.js
+++ crx/twitter_badges.js
@@ -3,13 +3,19 @@ chrome.storage.local.get(
     function (result) {
         var twitter_id_list = result['twitter_ids'];
         var usernames = document.getElementsByClassName('username');
+        console.log("Attempting to match.");
         for (var i = 0; i < usernames.length; i++) {
             var username = usernames[i].textContent;
             if(username.startsWith('@')) {
                 username = username.substr(1)
             }
             if(twitter_id_list.indexOf(username) != -1) {
-                usernames[i].textContent += " (RC)";
+                var logo = new Image();
+                logo.src = chrome.extension.getURL('rc.png');
+                logo.style.height = '1em';
+                logo.style['vertical-align'] = 'middle';
+                logo.style.margin = '2px';
+                usernames[i].appendChild(logo)
                 console.log("Match! " + username);
             }
         }
