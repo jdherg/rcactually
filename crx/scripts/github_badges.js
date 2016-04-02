96bb540c7a89966ba5fa32d1fb56fae36ca58d28
John Hergenroeder <jdherg@gmail.com>
2016-04-02 02:11:43 -0700

    Mark github usernames.

---
+++ crx/scripts/github_badges.js
@@ -0,0 +1,21 @@
+chrome.storage.local.get(
+    {'github_ids': []},
+    function (result) {
+        var github_id_list = result['github_ids'];
+        var links = document.getElementsByTagName('a');
+        console.log("Attempting to match.");
+        for (var i = 0; i < links.length; i++) {
+            var address = links[i].href;
+            var candidate = address.substr(address.indexOf('github.com') + 11).split('/')[0];
+            var gaClick = links[i].dataset.gaClick;
+            if(github_id_list.indexOf(candidate) != -1 && gaClick && gaClick.indexOf('target:actor') != -1) {
+                var logo = new Image();
+                logo.src = chrome.extension.getURL('images/rc.png');
+                logo.style.height = '1em';
+                logo.style['vertical-align'] = 'middle';
+                logo.style.margin = '2px';
+                links[i].appendChild(logo)
+                console.log("Match! " + candidate);
+            }
+        }
+})
